8180f20a26b28d8d083a67f08290b91cf4e3a158
MaTao <matao.mardle@gmail.com>
2015-05-08 17:14:24 +0800

    Update ValidPalindrome.java

--- Java/ValidPalindrome.java
+++ Java/ValidPalindrome.java
@@ -5,12 +5,14 @@
  *	"A man, a plan, a canal: Panama" is a palindrome.
  *	"race a car" is not a palindrome.
  *
- *	Note:
+ *  Note:
  *	Have you consider that the string might be empty? 
- *  This is a good question to ask during an interview.
+ *  	This is a good question to ask during an interview.
  *
  *	For the purpose of this problem, 
- *  we define empty string as valid palindrome.
+ *  	we define empty string as valid palindrome.
+ * 
+ * 	Author: matao
  */
 
 public class ValidPalindrome {
