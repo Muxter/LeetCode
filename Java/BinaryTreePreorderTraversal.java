04b1a0e8caa637994725509ff4cc9dc5c0070f22
SuperMardle <matao.mardle@gmail.com>
2015-05-12 13:57:33 +0800

    add Test case

--- Java/BinaryTreePreorderTraversal.java
+++ Java/BinaryTreePreorderTraversal.java
@@ -65,24 +65,56 @@ public class BinaryTreePreorderTraversal {
 	public static List<Integer> preorderTraversal3(TreeNode root) {
 		List<Integer> result = new ArrayList<Integer>();
 		Stack<TreeNode> stack = new Stack<TreeNode>();
-		if(root == null)
+
+		if(root == null) {
 			return result;
-		stack.push(root);
-		while(!stack.isEmpty()) {
-			TreeNode node = stack.pop();
-			result.add(node.val);
-			if(root.right != null) {
-				stack.push(node.right);
-			}
-			if(root.left != null) {
-				stack.push(node.left);
-			}
 		}
+
+		stack.push(root);
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
 		return result;
 	}  
 
 
     public static void main(String[] args) {
-    	
+    /**	1
+ 	 *   \
+ 	 *    2
+ 	 *   /
+ 	 *  3
+ 	 */
+    	TreeNode root = new TreeNode(1);
+    	TreeNode node1 = new TreeNode(2);
+    	TreeNode node2 = new TreeNode(3);
+    	root.right = node1;
+    	node1.left = node2;
+
+    	List<Integer> result1 = preorderTraversal1(root);
+    	for (Integer integer : result1) {
+    		System.out.print(integer + " ");
+    	}
+    	System.out.println();
+
+    	List<Integer> result2 = preorderTraversal2(root);
+    	for (Integer integer : result2) {
+    		System.out.print(integer + " ");
+    	}
+    	System.out.println();
+
+    	List<Integer> result3 = preorderTraversal3(root);
+    	for (Integer integer : result3) {
+    		System.out.print(integer + " ");
+    	}
+    	System.out.println();
     }
 }
