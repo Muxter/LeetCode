bb8a289e596be745645dea61a16f8857080136de
SuperMardle <matao.mardle@gmail.com>
2015-05-14 14:38:03 +0800

    add divide&conquer solution

--- Java/MaximumSubarray.java
+++ Java/MaximumSubarray.java
@@ -10,8 +10,8 @@
 
 public class MaximumSubarray {
 
-	// sliding window 滑动窗口
-	public static int maxSubArray(int[] nums) {
+	// Sliding Window 滑动窗口
+	public static int maxSubArray1(int[] nums) {
         if(nums == null || nums.length == 0) {
             return 0;
         }
@@ -28,8 +28,49 @@ public class MaximumSubarray {
         return maxSub;
     }
 
+    // Divide & Conquer
+    public static int maxSubArray2(int[] nums) {
+    	if(nums == null || nums.length == 0) {
+    		return 0;
+    	}
+    	return maxSubArrayHelper(nums, 0, nums.length - 1);
+    }
+
+    private static int maxSubArrayHelper(int[] nums, int left, int right) {
+    	if(left == right) {
+    		return nums[left];
+    	}
+
+    	int mid = left + (right - left) / 2;
+    	int maxLeft = maxSubArrayHelper(nums, left, mid);
+    	int maxRight = maxSubArrayHelper(nums, mid + 1, right);
+
+    	// 如果最大值跨过mid，部分左边，部分右边
+    	int maxTempLeft = nums[mid], sumTempLeft = nums[mid];
+    	for(int i = mid - 1; i >= left; i--) {
+    		sumTempLeft += nums[i];
+    		if(maxTempLeft < sumTempLeft) {
+    			maxTempLeft = sumTempLeft;
+    		}
+    	}
+    	int maxTempRight = nums[mid + 1], sumTempRight = nums[mid + 1];
+    	for(int j = mid + 2; j <= right; j++) {
+    		sumTempRight += nums[j];
+    		if(maxTempRight < sumTempRight) {
+    			maxTempRight = sumTempRight;
+    		}
+    	}
+    	int maxMid = maxTempLeft + maxTempRight;
+
+    	return (maxLeft > maxRight ? maxLeft : maxRight) > maxMid ? (maxLeft > maxRight ? maxLeft : maxRight) : maxMid;
+    }
+
 	public static void main(String[] args) {
 		int[] nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-		System.out.println(maxSubArray(nums1));
+		int[] nums2 = {2, 0, -3, 2, 1, 0, 1, -2};
+		System.out.println(maxSubArray1(nums1));
+		System.out.println(maxSubArray2(nums1));
+		System.out.println(maxSubArray1(nums2));
+		System.out.println(maxSubArray2(nums2));
 	}
 }
