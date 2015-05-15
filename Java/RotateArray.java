b7fbf4f9450be726d873b448c44e538889a2e15f
SuperMardle <matao.mardle@gmail.com>
2015-05-15 21:22:49 +0800

    bug fix

--- Java/RotateArray.java
+++ Java/RotateArray.java
@@ -25,11 +25,8 @@ public class RotateArray {
 		if (nums == null || nums.length == 0)
         	return;
         k = k % nums.length;
-        if (k == 0) {
-        	return;
-        }
-        reverse(nums, 0, k);
-        reverse(nums, k + 1, nums.length - 1);
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
         reverse(nums, 0, nums.length - 1);
 	}
 
@@ -45,7 +42,7 @@ public class RotateArray {
 
     public static void main(String[] args) {
     	int[] nums1 = {1, 2, 3, 4, 5, 6, 7};
-    	rotate(nums1, 0);
+    	rotate(nums1, 3);
     	for (int i = 0; i < nums1.length; i++) {
     		System.out.print(nums1[i] + " ");
     	}
