7c1bf9c742e801015966d2e563c0c4f02feb82d9
SuperMardle <matao.mardle@gmail.com>
2015-05-25 23:39:24 +0800

    add new solution

--- Java/ContainsDuplicate.java
+++ Java/ContainsDuplicate.java
@@ -11,7 +11,8 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class ContainsDuplicate {
-	public static boolean containsDuplicate(int[] nums) {
+	// HashMap
+	public static boolean containsDuplicate1(int[] nums) {
 		if(nums == null || nums.length == 0) {
 			return false;
 		}
@@ -26,10 +27,45 @@ public class ContainsDuplicate {
 		return false;
     }
 
+    // 排序后再判断, 顺便练习快排
+    public static boolean containsDuplicate2(int[] nums) {
+    	qSort(nums);
+    	for (int i = 0; i < nums.length - 1; i++) {
+    		if(nums[i] == nums[i+1]) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+
+    public static void qSort(int[] nums) {
+    	quickSort(nums, 0, nums.length - 1);
+    } 
+
+    public static void quickSort(int[] nums, int left, int right) {
+    	if(left < right) {
+    		int i = left, j = right;
+    		int pivot = nums[left];
+    		while(i < j && nums[j] >= pivot) j--;
+    		while(i < j && nums[i] <= pivot) i++;
+    		if(i < j) {
+    			int temp = nums[i];
+    			nums[i] = nums[j];
+    			nums[j] = temp;
+    		}
+    		nums[left] = nums[i];
+    		nums[i] = pivot;
+    		quickSort(nums, left, i - 1);
+    		quickSort(nums, j + 1, right);
+    	}
+    }
+
     public static void main(String[] args) {
     	int[] nums1 = {1, 2, 3, 3};
-    	int[] nums2 = {1, 3, 5, 6};
-    	System.out.println(containsDuplicate(nums1));
-    	System.out.println(containsDuplicate(nums2));
+    	int[] nums2 = {1, 5, 2, 3, 6};
+    	System.out.println(containsDuplicate1(nums1));
+    	System.out.println(containsDuplicate1(nums2));
+    	System.out.println(containsDuplicate2(nums1));
+    	System.out.println(containsDuplicate2(nums2));
     }
 }
