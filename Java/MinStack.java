307387f200425f3402953c1661db6ad7bf65b223
SuperMardle <matao.mardle@gmail.com>
2015-06-13 23:46:18 +0800

    add Min Stack solution

---
+++ Java/MinStack.java
@@ -0,0 +1,32 @@
+/**
+ *	Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ *
+ *	push(x) -- Push element x onto stack.
+ *	pop() -- Removes the element on top of the stack.
+ *	top() -- Get the top element.
+ *	getMin() -- Retrieve the minimum element in the stack.
+ *
+ *	Author: matao
+ */
+
+public class MinStack {
+	public static void push(int x) {
+        
+    }
+
+    public static void pop() {
+        
+    }
+
+    public static int top() {
+        
+    }
+
+    public static int getMin() {
+        
+    }
+
+    public static void main(String[] args) {
+    	
+    }
+}
