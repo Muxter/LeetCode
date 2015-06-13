69be35ed9b124c6efb45c2daa5925abba7a5aad1
SuperMardle <matao.mardle@gmail.com>
2015-06-13 23:58:23 +0800

    add Min Stack solution

--- Java/MinStack.java
+++ Java/MinStack.java
@@ -9,24 +9,40 @@
  *	Author: matao
  */
 
+import java.util.Stack;
+
 public class MinStack {
-	public static void push(int x) {
-        
-    }
 
+	private static Stack<Integer> dataStack = new Stack<Integer>();
+    private static Stack<Integer> minStack = new Stack<Integer>();
+     
+    public static void push(int node) {
+        dataStack.push(node);
+        if(minStack.isEmpty() || minStack.peek() > node) {
+            minStack.push(node);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+     
     public static void pop() {
-        
+        dataStack.pop();
+        minStack.pop();
     }
-
+     
     public static int top() {
-        
+        return dataStack.peek();
     }
-
+     
     public static int getMin() {
-        
+        return minStack.peek();
     }
 
     public static void main(String[] args) {
-    	
-    }
+		MinStack mStack = new MinStack();
+		mStack.push(2);
+		mStack.push(3);
+		mStack.push(4);
+		System.out.println(mStack.getMin());
+	}
 }
