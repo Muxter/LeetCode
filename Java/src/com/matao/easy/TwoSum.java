a0118d5091328efc5bc047947665d4ced4771558
matao <muxter.matao@gmail.com>
2018-09-30 10:37:39 +0800

    feat(TwoSum): add solution to 1-TwoSum

---
+++ Java/src/com/matao/easy/TwoSum.java
@@ -0,0 +1,36 @@
+package com.matao.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by matao on 2018/9/30.
+ * <p>
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <br><br>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int[] result = twoSum(nums, 9);
+        System.out.println(Arrays.toString(result));
+    }
+}
