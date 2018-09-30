f395616c8f0548828bedb8438f993ad60ac96d78
matao <muxter.matao@gmail.com>
2018-09-30 11:17:21 +0800

    feat(ListNode): add common ListNode

---
+++ Java/src/com/matao/ListNode.java
@@ -0,0 +1,13 @@
+package com.matao;
+
+/**
+ * Created by matao on 2018/9/30.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
---
+++ Java/src/com/matao/medium/AddTwoNumbers.java
@@ -0,0 +1,7 @@
+package com.matao.medium;
+
+/**
+ * Created by matao on 2018/9/30.
+ */
+public class AddTwoNumbers {
+}
